301436ce662d7b0f9f739f9917a22cab7da2bac7
LuccaRendall <luccarendall2@gmail.com>
2023-09-20 00:05:59 -0300

    feat: Implementa a função para calcular a aŕea de um retângulo

--- functions.ts
+++ functions.ts
@@ -1,3 +1,9 @@
+// Area quadrado = lado²
 export function getSquareArea(side: number): number {
   return side ** 2;
+}
+
+// Área retângulo = base * altura
+export function getRectangleArea(base: number, height: number): number {
+  return base * height;
 }
