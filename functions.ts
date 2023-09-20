c0278fc37d1d4f64641c108c72ffe7610f85e312
LuccaRendall <luccarendall2@gmail.com>
2023-09-20 00:12:23 -0300

    feat: Implementa a função para calcular o perímetro de um polígono regular qualquer

--- functions.ts
+++ functions.ts
@@ -13,3 +13,8 @@ export function getTriangleArea(base: number, height: number): number {
   return (base * height) / 2;
 }
 
+// Perímetro de um polígono = ladoA + ladoB + ladoC + ladoD + ladoE + ladoF ...
+export function getPolygonPerimeter(sides: number[]): number {
+  return sides.reduce((acumulador, side) => acumulador + side, 0); // função reduce que itera pelos elementos do array sides e acumula seus valores
+} // (acumulador, side) => acumulador + side é uma função de callback usada na redução. O acumulador começa com o valor inicial de 0, e side é o valor atual do array sendo percorrido. A função de callback simplesmente adiciona o valor side ao acumulador em cada iteração, somando todos os comprimentos dos lados do polígono.
+
